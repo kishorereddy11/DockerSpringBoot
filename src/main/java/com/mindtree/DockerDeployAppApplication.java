ab15eeb2c05b61d867c259b2fb4eb870f39467a9
Thippareddygari Omnandhakishorreddy <M1054492@mindtree.com>
2021-04-10 18:47:19 +0530

    first commit

---
+++ src/main/java/com/mindtree/DockerDeployAppApplication.java
@@ -0,0 +1,13 @@
+package com.mindtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDeployAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDeployAppApplication.class, args);
+	}
+
+}
